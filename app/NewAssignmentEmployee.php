200ec81fc3961b4f40c638ab9587d652e2697991
yogi <dimas.yogi.777@gmail.com>
2021-06-02 08:57:51 +0700

    asd

--- app/NewAssignmentEmployee.php
+++ app/NewAssignmentEmployee.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Observers\BlameableObserver;
-use App\Traits\Blameable;
+use App\Blameable;
 
 class NewAssignmentEmployee extends Model
 {

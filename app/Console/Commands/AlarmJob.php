77d0bc4fa11b94160d2e34d95f061fdcc77c72bb
yogi <dimas.yogi.777@gmail.com>
2021-06-14 10:57:37 +0700

    ad

--- app/Console/Commands/AlarmJob.php
+++ app/Console/Commands/AlarmJob.php
@@ -41,7 +41,7 @@ class AlarmJob extends Command
      */
     public function handle()
     {
-        $user = User::where([['role_id',"=",4],['status',"=",1]])->get();
+        $users = User::where([['role_id',"=",4],['status',"=",1]])->get();
         $description = Alarm::first()->toArray();
         foreach ($users as $user) {
             Mail::send('emails.alarm', ["data" =>  $description], function ($msg) use ($user) {
